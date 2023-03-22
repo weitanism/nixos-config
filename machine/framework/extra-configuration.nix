a8bca72c020ea1dfe88f7ecfbe0ea0f307b4c1c2
weitanism <chrisyunhua@gmail.com>
2023-03-22 15:49:14 +0800

    fix: Add kernel param to fix freezing issue

--- machine/framework/extra-configuration.nix
+++ machine/framework/extra-configuration.nix
@@ -6,6 +6,12 @@
   ];
 
   boot.supportedFilesystems = [ "ntfs" ];
+  boot.kernelParams = [
+    # Fix the random freezing issue(dmesg says "[drm] GPU HANG: ecode
+    # 12:0:00000000"). See
+    # https://community.frame.work/t/tracking-hard-freezing-on-fedora-36-with-the-new-12th-gen-system/20675/146
+    "i915.enable_psr=0"
+  ];
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;

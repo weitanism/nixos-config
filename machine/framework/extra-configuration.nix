4531f18a04606f708730ddd1d00ffba23c2e7981
weitanism <chrisyunhua@gmail.com>
2023-04-25 10:34:36 +0800

    feat: Enable proxy on nix daemon

--- machine/framework/extra-configuration.nix
+++ machine/framework/extra-configuration.nix
@@ -24,8 +24,8 @@
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  # systemd.services.nix-daemon.environment = {
-  #   http_proxy = "http://127.0.0.1:1080";
-  #   https_proxy = "http://127.0.0.1:1080";
-  # };
+  systemd.services.nix-daemon.environment = {
+    http_proxy = "http://127.0.0.1:1080";
+    https_proxy = "http://127.0.0.1:1080";
+  };
 }

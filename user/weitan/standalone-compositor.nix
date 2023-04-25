03d6e80508bc566907895cad4feca977f2a16341
weitanism <chrisyunhua@gmail.com>
2023-04-25 15:14:46 +0800

    fix: Prevent hidden windows from being background

--- user/weitan/standalone-compositor.nix
+++ user/weitan/standalone-compositor.nix
@@ -10,8 +10,16 @@ in
     fade = true;
     fadeDelta = 5;
     inactiveOpacity = 0.8;
+    activeOpacity = 0.9;
     opacityRules = [
+      # Set opacity of hidden windows to 0. This rule should be put on
+      # the first place as picom only use the first matching rule.
+      # See https://github.com/yshui/picom/issues/317 and
+      # https://www.reddit.com/r/i3wm/comments/mehr01/weirdness_with_tabbed_transparent_windows_picom/
+      "0:_NET_WM_STATE@:32a = '_NET_WM_STATE_HIDDEN'"
+
       "100:class_g = 'Rofi'"
+      "100:class_g = 'Chromium-browser'"
     ];
     settings = {
       corner-radius = 12.0;
